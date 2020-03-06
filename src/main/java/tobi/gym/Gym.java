0bb2863c2901314187ea8e90acfc63779578fec2
tobq <tobi.akiny@gmail.com>
2020-03-06 15:24:22 +0000

    turned off threading while issue remains

--- src/main/java/tobi/gym/Gym.java
+++ src/main/java/tobi/gym/Gym.java
@@ -27,7 +27,7 @@ public class Gym implements AutoCloseable {
     private static final String GYM_PYTHON_FOLDER = "/tobi/gym/gympy/";
     private static final String SHELL_PY_REL_PATH = "shell.py";
     private static final String PYTHON_SHELL_PATH = GYM_PYTHON_FOLDER + SHELL_PY_REL_PATH;
-    private static final boolean THREADED_PYTHON_EXECUTION = true;
+    private static final boolean THREADED_PYTHON_EXECUTION = false;
 
     private Runnable shutdownUninstall;
     private final Runnable shutdownDestroySubprocess;
